23b5ccfcc9e73e96ed0e7d6eae200e7e56abf221
niedo <57731234+ni3do@users.noreply.github.com>
2022-10-28 08:21:00 +0000

    added parameters on top, multiple column drop, Iterative Imputer added

--- task1/main.py
+++ task1/main.py
@@ -1,4 +1,3 @@
-from ossaudiodev import SNDCTL_COPR_WCODE
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
@@ -11,14 +10,26 @@ from sklearn.mixture import GaussianMixture
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.ensemble import GradientBoostingRegressor
 import warnings
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 
 warnings.filterwarnings("ignore")
 
+IMP_NN = 100
+LOAD_IMP_DATA = True
+GM_COMPONENTS = 100
+PERC_THRESHOLD = 10
+LASSO_ALPHA = 0.1
+GBR_ESTIMATORS = 200
+D_COLS_ROUND = 16
 
 if __name__ == "__main__":
 
     # Load data
-    x_train = pd.read_csv("data/X_train.csv")
+    if LOAD_IMP_DATA:
+        x_train = pd.read_csv("data/x_train_imp.csv")
+    else:
+        x_train = pd.read_csv("data/X_train.csv")
 
     test = pd.read_csv("data/X_test.csv")
     y_train = pd.read_csv("data/y_train.csv")
@@ -26,76 +37,80 @@ if __name__ == "__main__":
     pred = pd.DataFrame(test["id"])
 
     # Remove nan values
-    imp = SimpleImputer(missing_values=np.nan, strategy="mean")
+    imp = IterativeImputer(n_nearest_features=IMP_NN, imputation_order="random", random_state=0)
 
-    clf = KernelRidge(alpha=10.0, kernel="polynomial", degree=3, coef0=2.5)
-    # clf = Lasso(alpha=0)
+    x_train = imp.fit_transform(x_train)
+
+    pd.DataFrame(x_train).to_csv("data/x_train_imp.csv", index=False)
 
     # normalize data
     x_train = preprocessing.StandardScaler().fit_transform(x_train)
     y_train = y_train.to_numpy()
-    # y_train = preprocessing.StandardScaler().fit_transform(y_train)
 
     # make train test split
     x_train, x_test, y_train, y_test = train_test_split(
         x_train, y_train, test_size=0.2, random_state=42
     )
-    
 
+    # delete id column
     x_train = np.delete(x_train, axis=1, obj=0)
     x_test = np.delete(x_test, axis=1, obj=0)
     y_train = np.squeeze(np.delete(y_train, axis=1, obj=0))
     y_test = np.squeeze(np.delete(y_test, axis=1, obj=0))
-    
 
-    x_train = imp.fit_transform(x_train)
-    x_test = imp.transform(x_test)
-
-    gm = GaussianMixture(n_components=100, random_state=0).fit(x_train)
+    gm = GaussianMixture(n_components=GM_COMPONENTS, random_state=0).fit(x_train)
     score = gm.score_samples(x_train)
 
     # Get the score threshold for anomaly
-    pct_threshold = np.percentile(score, 10)  # Print the score threshold
+    pct_threshold = np.percentile(score, PERC_THRESHOLD)  # Print the score threshold
     print(f"The threshold of the score is {pct_threshold:.2f}")  # Label the anomalies
-    # outliers = score.apply(lambda x: 1 if x < pct_threshold else 0)
     outliers = score > pct_threshold
-    print(len(np.nonzero(outliers)[0]))
 
     x_train = x_train[np.nonzero(outliers)[0]]
     y_train = y_train[np.nonzero(outliers)[0]]
-
 
     k = x_train.shape[1] - 1
 
     best_score = -1
     best_columns = 0
     columns_to_drop = []
-    for i in range(k):
-        model = Lasso(alpha=0.1)
+
+    for i in range(0, k, D_COLS_ROUND):
+        model = Lasso(alpha=LASSO_ALPHA)
         model.fit(x_train, y_train)
         coef = model.coef_
-        idx = np.argmin(np.abs(coef))
-        columns_to_drop.append(idx)
 
-        dropped_column = x_train[:, [idx]]
-        print("Variance of dropped column_ {}".format(np.var(dropped_column)))
+        print(f"Dropping columns: ")
 
-        x_train = np.delete(x_train, idx, 1)
-        x_test = np.delete(x_test, idx, 1)
+        for j in range(D_COLS_ROUND):
+            if i + j >= k:
+                continue
 
-        # clf = KernelRidge(alpha=10.0, kernel="polynomial", degree=3, coef0=2.5)
-        clf = GradientBoostingRegressor(loss="squared_error", n_estimators=200)
+            idx = np.argmin(np.abs(coef))
+            coef = np.delete(coef, idx)
+
+            columns_to_drop.append(idx)
+            dropped_column = x_train[:, [idx]]
+
+            x_train = np.delete(x_train, idx, 1)
+            x_test = np.delete(x_test, idx, 1)
+
+        clf = GradientBoostingRegressor(loss="squared_error", n_estimators=GBR_ESTIMATORS)
         clf.fit(x_train, y_train)
+        
+        print(f"New column count: {len(coef)}")
         score = r2_score(y_test, clf.predict(x_test))
         if best_score < score:
             best_score = score
-            best_dropped_columns = i+1
-        print("Dropped columns: {} R2 score: {}".format(i+1, score))
+            best_dropped_columns = i + 1
+            pd.DataFrame(x_train).to_csv("best_train.csv", index=False)
+            pd.DataFrame(x_test).to_csv("best_test.csv", index=False)
 
-    print("Best score: {}".format(best_score)) 
+        print("Dropped columns: {} R2 score: {}".format(i + 1, score))
+        pd.DataFrame(columns_to_drop).to_csv("columns_to_drop.csv", index=False)
+
+    print("Best score: {}".format(best_score))
     print("Best dropped columns: {}".format(best_dropped_columns))
-
-    pd.DataFrame(columns_to_drop).to_csv('columns_to_drop.csv')
 
     # sel = VarianceThreshold(threshold=(0.95))
     # x_train = sel.fit_transform(x_train, y_train)
