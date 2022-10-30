c19ef514cb1e83532f2f96b16061976f6093df05
Yannick Wattenberg <wattenberg@mailbox.org>
2022-10-30 12:56:54 +0100

    refactor out utils add cross for feature selection

--- task1/main.py
+++ task1/main.py
@@ -2,7 +2,6 @@ import os
 import warnings
 import json
 
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,75 +15,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.mixture import GaussianMixture
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.experimental import enable_iterative_imputer
-from sklearn.impute import IterativeImputer
+
 from sklearn.model_selection import KFold
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_regression
+from sklearn.feature_selection import mutual_info_regression
+
+import utils
 
 warnings.filterwarnings("ignore")
 
 IMP_NN = 32
-LOAD_IMP_DATA = True
-GM_COMPONENTS = 140
+GM_COMPONENTS = 120
 PERC_THRESHOLD = 10
 LASSO_ALPHA = 5
 GBR_ESTIMATORS = 200
 D_COLS_ROUND = 16
-
-
-def get_imputer(x_train, max_iter=10, k_neighbors=IMP_NN):
-    imp = IterativeImputer(
-        max_iter=max_iter, n_nearest_features=k_neighbors, random_state=0
-    )
-    imp.fit(x_train)
-    return imp
-
-
-def load_data(use_imp_data=False, safe_imp_data=True):
-    if use_imp_data:
-        x_train_imp = pd.read_csv("data/x_train_imp.csv")
-        test_imp = pd.read_csv("data/test_imp.csv")
-        y_train = pd.read_csv("data/y_train.csv").drop("id", axis=1)
-
-        id = test_imp["id"].to_numpy()
-        test_imp = test_imp.drop("id", axis=1).to_numpy()
-
-        return x_train_imp.to_numpy(), test_imp, id, y_train.to_numpy()
-
-    else:
-        x_train = pd.read_csv("data/X_train.csv").drop("id", axis=1)
-        y_train = pd.read_csv("data/y_train.csv").drop("id", axis=1).to_numpy()
-        test = pd.read_csv("data/X_test.csv")
-
-        id = test["id"].to_numpy()
-        test = test.drop("id", axis=1)
-
-        imp = get_imputer(x_train)
-        x_train_imp = imp.transform(x_train)
-        test_imp = imp.transform(test)
-
-        if safe_imp_data:
-            pd.DataFrame(x_train_imp).to_csv("data/x_train_imp.csv", index=False)
-            test_df = pd.DataFrame(test_imp)
-            test_df["id"] = id
-            test_df.to_csv("data/test_imp.csv", index=False)
-        return x_train_imp, test_imp, id, y_train
-
-
-def train_outlier_detection_model(x_train, n_components=GM_COMPONENTS, random=True):
-    if random:
-        gm = GaussianMixture(n_components=n_components)
-    else:
-        gm = GaussianMixture(n_components=n_components, random_state=0)
-    gm.fit(x_train)
-    return gm
-
-
-def get_outlier_mask(x_train, gm, threshold=PERC_THRESHOLD, print_stats=False):
-    scores = gm.score_samples(x_train)
-    perc = np.percentile(scores, threshold)
-    if print_stats:
-        print(f"The threshold of the score is {perc:.2f}")
-    return scores > perc
 
 
 def cross_validation_gmm_components(
@@ -103,20 +49,20 @@ def cross_validation_gmm_components(
         score = 0
         print(f"Components: {components}")
         for train_index, test_index in kf.split(x_train):
-            x_train_cv, x_test_cv = x_train[train_index], x_train[test_index]
-            y_train_cv, y_test_cv = y_train[train_index], y_train[test_index]
-            gm = train_outlier_detection_model(x_train_cv, n_components=components)
+            x_train_cv, x_test_cv, y_train_cv, y_test_cv = get_split_from_index(
+                x_train, y_train, train_index, test_index
+            )
+            gm = utils.train_outlier_detection_model(
+                x_train_cv, n_components=components
+            )
 
-            mask = get_outlier_mask(x_train_cv, gm, threshold)
+            mask = utils.get_outlier_mask(x_train_cv, gm, threshold)
             x_train_cv = x_train_cv[np.nonzero(mask)[0]]
             y_train_cv = y_train_cv[np.nonzero(mask)[0]]
 
-            clf = GradientBoostingRegressor(
-                loss="squared_error", n_estimators=regressor_steps
+            curr_score = utils.get_r2_score(
+                x_train_cv, x_test_cv, y_train_cv, y_test_cv, regressor_steps
             )
-            clf.fit(x_train_cv, y_train_cv)
-            y_pred = clf.predict(x_test_cv)
-            curr_score = r2_score(y_test_cv, y_pred)
             score += curr_score
             print(f"Current score {components} components: {curr_score:.4f}")
         scores[score / num_of_splits] = components
@@ -143,26 +89,24 @@ def cross_validation_gmm_threshold(
     for train_index, test_index in kf.split(x_train):
         x_train_cv = x_train[train_index]
         models.append(
-            train_outlier_detection_model(x_train_cv, n_components=n_components)
+            utils.train_outlier_detection_model(x_train_cv, n_components=n_components)
         )
 
     for threshold in range(begin, end, step):
         score = 0
         print(f"Threshold: {threshold}%")
         for idx, (train_index, test_index) in enumerate(kf.split(x_train)):
-            x_train_cv, x_test_cv = x_train[train_index], x_train[test_index]
-            y_train_cv, y_test_cv = y_train[train_index], y_train[test_index]
+            x_train_cv, x_test_cv, y_train_cv, y_test_cv = utils.get_split_from_index(
+                x_train, y_train, train_index, test_index
+            )
 
-            mask = get_outlier_mask(x_train_cv, models[idx], threshold)
+            mask = utils.get_outlier_mask(x_train_cv, models[idx], threshold)
             x_train_cv = x_train_cv[np.nonzero(mask)[0]]
             y_train_cv = y_train_cv[np.nonzero(mask)[0]]
 
-            clf = GradientBoostingRegressor(
-                loss="squared_error", n_estimators=regressor_steps
+            curr_score = utils.get_r2_score(
+                x_train_cv, x_test_cv, y_train_cv, y_test_cv, regressor_steps
             )
-            clf.fit(x_train_cv, y_train_cv)
-            y_pred = clf.predict(x_test_cv)
-            curr_score = r2_score(y_test_cv, y_pred)
             score += curr_score
             print(f"Current score with threshold {threshold}%: {curr_score:.4f}")
         scores[score / num_of_splits] = threshold
@@ -187,15 +131,12 @@ def cross_validation_regression_num_regressors(
         score = 0
         print(f"Regressor steps: {regressors}")
         for train_index, test_index in kf.split(x_train):
-            x_train_cv, x_test_cv = x_train[train_index], x_train[test_index]
-            y_train_cv, y_test_cv = y_train[train_index], y_train[test_index]
-
-            clf = GradientBoostingRegressor(
-                loss="squared_error", n_estimators=regressors
+            x_train_cv, x_test_cv, y_train_cv, y_test_cv = utils.get_split_from_index(
+                x_train, y_train, train_index, test_index
             )
-            clf.fit(x_train_cv, y_train_cv)
-            y_pred = clf.predict(x_test_cv)
-            curr_score = r2_score(y_test_cv, y_pred)
+            curr_score = utils.get_r2_score(
+                x_train_cv, x_test_cv, y_train_cv, y_test_cv, regressors
+            )
             score += curr_score
             print(f"Current score {regressors} regressors: {curr_score:.4f}")
         scores[score / num_of_splits] = regressors
@@ -205,44 +146,43 @@ def cross_validation_regression_num_regressors(
     return scores
 
 
-def filter_outliers(
-    x_train, y_train, threshold=PERC_THRESHOLD, n_components=GM_COMPONENTS
+def cross_validation_feature_selection(
+    x_train,
+    y_train,
+    num_of_splits=5,
+    regressor_steps=GBR_ESTIMATORS,
+    begin=10,
+    end=800,
+    step=10,
 ):
-    gm = train_outlier_detection_model(x_train, n_components=n_components)
-    mask = get_outlier_mask(x_train, gm, threshold)
-    return x_train[np.nonzero(mask)[0]], y_train[np.nonzero(mask)[0]]
-
-
-def dump_scores_to_file(scores, path):
-    if os.path.exists(path):
-        with open(path, "r+") as f:
-            data = json.load(f)
-            runs = data["runs"]
-            runs[len(runs)] = scores
-            data["optimal"].append(scores[max(scores.keys())])
-            f.seek(0)
-            json.dump(data, f, indent=4)
-    else:
-        with open(path, "w") as f:
-            data = {"optimal": [scores[max(scores.keys())]], "runs": {0: scores}}
-            json.dump(data, f, indent=4)
-
-
-def safe_cross_scores(
-    component_scores=None, threshold_scores=None, regressor_scores=None
-):
-    if component_scores is not None:
-        dump_scores_to_file(component_scores, "gmm_components.json")
-    if threshold_scores is not None:
-        dump_scores_to_file(threshold_scores, "gmm_threshold.json")
-    if regressor_scores is not None:
-        dump_scores_to_file(regressor_scores, "regressor_num.json")
+    scores = {}
+    kf = KFold(n_splits=num_of_splits, shuffle=True)
+    for features in range(begin, end, step):
+        score = 0
+        print(f"Number of features: {features}")
+        for train_index, test_index in kf.split(x_train):
+            x_train_cv, x_test_cv, y_train_cv, y_test_cv = utils.get_split_from_index(
+                x_train, y_train, train_index, test_index
+            )
+            x_train_cv, x_test_cv = utils.select_features(
+                x_train_cv, y_train_cv, x_test_cv, features, use_mutual_info=True
+            )
+            curr_score = utils.get_r2_score(
+                x_train_cv, x_test_cv, y_train_cv, y_test_cv, regressor_steps
+            )
+            score += curr_score
+            print(f"Current score {features} features: {curr_score:.4f}")
+        scores[score / num_of_splits] = features
+        print("-" * 50)
+        print(f"Final score for {features} features: {score / num_of_splits:.4f}")
+        print("-" * 50)
+    return scores
 
 
 if __name__ == "__main__":
 
     # Load data
-    x_train, test, test_id, y_train = load_data(use_imp_data=True)
+    x_train, test, test_id, y_train = utils.load_data(use_imp_data=True)
     print("Data loaded")
 
     # Normalize data
@@ -251,34 +191,40 @@ if __name__ == "__main__":
     test = scaler.transform(test)
 
     # Select outlier detection model
-    component_scores = cross_validation_gmm_components(x_train, y_train)
-    print("Finished cross validation for components")
-    best_num_of_components = component_scores[max(component_scores.keys())]
-    print(f"Best model has {best_num_of_components} components")
+    # component_scores = cross_validation_gmm_components(x_train, y_train)
+    # print("Finished cross validation for components")
+    # best_num_of_components = component_scores[max(component_scores.keys())]
+    # print(f"Best model has {best_num_of_components} components")
 
-    threshold_scores = cross_validation_gmm_threshold(
-        x_train, y_train, n_components=best_num_of_components
-    )
-    print("Finished cross validation for threshold")
-    best_threshold = threshold_scores[max(threshold_scores.keys())]
-    print(f"Best model has {best_threshold} threshold")
+    # threshold_scores = cross_validation_gmm_threshold(
+    #     x_train, y_train, n_components=best_num_of_components
+    # )
+    # print("Finished cross validation for threshold")
+    # best_threshold = threshold_scores[max(threshold_scores.keys())]
+    # print(f"Best model has {best_threshold} threshold")
 
-    print(
-        f"Filtering outliers with {best_num_of_components} components and threshold {best_threshold}%"
-    )
+    # print(
+    #     f"Filtering outliers with {best_num_of_components} components and threshold {best_threshold}%"
+    # )
 
-    x_train, y_train = filter_outliers(
-        x_train, y_train, threshold=best_threshold, n_components=best_num_of_components
-    )
+    # x_train, y_train = filter_outliers(
+    #     x_train, y_train, threshold=best_threshold, n_components=best_num_of_components
+    # )
 
-    num_regressor_score = cross_validation_regression_num_regressors(x_train, y_train)
-    print("Finished cross validation for number of regressors")
-    best_num_of_regressors = num_regressor_score[max(num_regressor_score.keys())]
-    print(f"Best model has {best_num_of_regressors} regressors")
+    # num_regressor_score = cross_validation_regression_num_regressors(x_train, y_train)
+    # print("Finished cross validation for number of regressors")
+    # best_num_of_regressors = num_regressor_score[max(num_regressor_score.keys())]
+    # print(f"Best model has {best_num_of_regressors} regressors")
+    # scores_map = {"component_scores":component_scores, "threshold_scores":threshold_scores, "num_regressor_score":num_regressor_score}
+    # utils.safe_cross_scores(scores_map)
+    ## Train model
 
-    safe_cross_scores(component_scores, threshold_scores, num_regressor_score)
-    # Train model
-
+    feature_selection_scores = cross_validation_feature_selection(x_train, y_train)
+    print("Finished cross validation for feature selection")
+    best_num_of_features = feature_selection_scores[
+        max(feature_selection_scores.keys())
+    ]
+    print(f"Best model has {best_num_of_features} features")
     quit()
 
     k = x_train.shape[1] - 1
