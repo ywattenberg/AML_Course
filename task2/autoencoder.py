77a21d3099015cebc6f8eb85052a31ed565f72f1
Yannick Wattenberg <wattenberg@mailbox.org>
2022-11-20 15:59:09 +0100

    start autoencoder

---
+++ task2/autoencoder.py
@@ -0,0 +1,76 @@
+from torch import nn
+from torch.nn import functional as F
+
+class Encoder(nn.Module):
+
+  def __init__(self, seq_len, n_features, embedding_dim=1024, num_layers=2, hidden_dim=2*1024):
+    super(Encoder, self).__init__()
+    self.seq_len, self.n_features = seq_len, n_features
+    self.embedding_dim, self.hidden_dim = embedding_dim, hidden_dim
+    
+    self.rnn1 = nn.LSTM(
+      input_size=n_features,
+      hidden_size=self.hidden_dim,
+      num_layers=num_layers,
+      batch_first=True
+    )
+
+    self.rnn2 = nn.LSTM(
+      input_size=self.hidden_dim,
+      hidden_size=embedding_dim,
+      num_layers=num_layers,
+      batch_first=True
+    )
+
+  def forward(self, x):
+    x = x.reshape((1, self.seq_len, self.n_features))
+    x, (_, _) = self.rnn1(x)
+    x, (hidden_n, _) = self.rnn2(x)
+    return hidden_n.reshape((self.n_features, self.embedding_dim))
+
+class Decoder(nn.Module):
+
+   def __init__(self, seq_len, n_features, embedding_dim=1024, num_layers=2, hidden_dim=2*1024):
+    super(Encoder, self).__init__()
+    self.seq_len, self.n_features = seq_len, n_features
+    self.embedding_dim, self.hidden_dim = embedding_dim, hidden_dim
+    
+    self.rnn1 = nn.LSTM(
+      input_size=embedding_dim,
+      hidden_size=self.hidden_dim,
+      num_layers=num_layers,
+      batch_first=True
+    )
+
+    self.rnn2 = nn.LSTM(
+      input_size=self.hidden_dim,
+      hidden_size=embedding_dim,
+      num_layers=num_layers,
+      batch_first=True
+    )
+
+    self.output_layer = nn.Linear(self.hidden_dim, n_features)
+
+  def forward(self, x):
+    x = x.repeat(self.seq_len, self.n_features)
+    x = x.reshape((self.n_features, self.seq_len, self.input_dim))
+    x, (hidden_n, cell_n) = self.rnn1(x)
+    x, (hidden_n, cell_n) = self.rnn2(x)
+    x = x.reshape((self.seq_len, self.hidden_dim))
+    return self.output_layer(x)
+
+class RecurrentAutoencoder(nn.Module):
+    def __init__(self, seq_len, n_features):
+        super(RecurrentAutoencoder, self).__init__()
+        self.seq_len = seq_len
+        self.n_features = n_features
+
+        self.encoder = Encoder(seq_len, n_features).to(args.device)
+        self.decoder = Decoder(seq_len, n_features).to(args.device)
+    def forward(self, x):
+        x = self.encoder(x)
+
+        x = self.decoder(x)
+        return x
+
+
