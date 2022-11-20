85d265d72c3aa9a5aece3df0b09ebd8912f9db7d
niedo <57731234+ni3do@users.noreply.github.com>
2022-11-20 16:08:11 +0100

    print simple results to file

--- task2/main_simple.py
+++ task2/main_simple.py
@@ -22,7 +22,7 @@ def gridsearch(model, param_grid, X_train, y_train):
     print("Best cross-validation score: {:.2f}".format(grid.best_score_))
     print("Best parameters: ", grid.best_params_)
     print("Test set score: {:.2f}".format(grid.score(X_val, y_val)))
-    return grid
+    return grid, X_val, y_val
 
 
 def main():
@@ -33,9 +33,11 @@ def main():
 
     # X_train_1 = utils.process_data(X_train)
     # X_test_1 = utils.process_data(X_test)
+    # print("process data done")
 
     # X_train_2 = utils.process_data2(X_train)
     # X_test_2 = utils.process_data2(X_test)
+    # print("process data2 done")
 
     # imputer = SimpleImputer(strategy="median", fill_value=0).fit(X_train_2)
     # X_train_2 = imputer.transform(X_train_2)
@@ -43,11 +45,13 @@ def main():
     # scaler = StandardScaler().fit(X_train_2)
     # X_train_2 = scaler.transform(X_train_2)
     # X_test_2 = scaler.transform(X_test_2)
+    # print("imputer and scaler done")
 
     # X_train_con = np.concatenate((X_train_1, X_train_2), axis=1)
     # X_test_con = np.concatenate((X_test_1, X_test_2), axis=1)
+    # print("concatenate done")
 
-    y_train = y_train["y"].to_numpy().flatten()
+    # y_train = y_train["y"].to_numpy().flatten()
 
     # pd.DataFrame(X_train_1).to_csv("data/X_train_1.csv", index=False)
     # pd.DataFrame(X_test_1).to_csv("data/X_test_1.csv", index=False)
@@ -55,6 +59,7 @@ def main():
     # pd.DataFrame(X_test_2).to_csv("data/X_test_2.csv", index=False)
     # pd.DataFrame(X_train_con).to_csv("data/X_train_con.csv", index=False)
     # pd.DataFrame(X_test_con).to_csv("data/X_test_con.csv", index=False)
+    # print("save done")
 
     X_train_1 = pd.read_csv("data/X_train_1.csv")
     X_test_1 = pd.read_csv("data/X_test_1.csv")
@@ -65,27 +70,34 @@ def main():
 
     param_grid = {
         "n_estimators": [200, 400, 600, 800, 1000],
-        "max_depth": [3, 5, 7],
-        "learning_rate": [0.01, 0.05, 0.1, 0.2],
+        "learning_rate": [0.01, 0.1, 0.5, 1.0, 1.5, 2.0],
     }
-    gridsearch(
-        model=XGBClassifier(objective="multi:softprob"),
+    grid, X_val, y_val = gridsearch(
+        model=AdaBoostClassifier(),
         param_grid=param_grid,
         X_train=X_train_1,
         y_train=y_train,
     )
-    gridsearch(
-        model=XGBClassifier(objective="multi:softprob"),
+    with open("result.txt", "a") as f:
+        f.write(f"X_train_1,{grid.best_score_},{grid.best_params_},{grid.score(X_val, y_val)}")
+
+    grid, X_val, y_val = gridsearch(
+        model=AdaBoostClassifier(),
         param_grid=param_grid,
         X_train=X_train_2,
         y_train=y_train,
     )
-    gridsearch(
-        model=XGBClassifier(objective="multi:softprob"),
+    with open("result.txt", "a") as f:
+        f.write(f"X_train_2,{grid.best_score_},{grid.best_params_},{grid.score(X_val, y_val)}")
+
+    grid, X_val, y_val = gridsearch(
+        model=AdaBoostClassifier(),
         param_grid=param_grid,
         X_train=X_train_con,
         y_train=y_train,
     )
+    with open("result.txt", "a") as f:
+        f.write(f"X_train_con,{grid.best_score_},{grid.best_params_},{grid.score(X_val, y_val)}")
 
     # XGBClassifier_con = XGBClassifier(max_depth=5, n_estimators=500, learning_rate=0.05).fit(X_train_con, y_train)
     # XGBClassifier_1 = XGBClassifier(max_depth=5, n_estimators=250, learning_rate=0.05).fit(X_train_1, y_train)
