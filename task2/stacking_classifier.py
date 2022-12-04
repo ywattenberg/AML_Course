2dec89aeb0345e829b9e3b708a71491020af1618
niedo <57731234+ni3do@users.noreply.github.com>
2022-12-04 19:57:14 +0100

    added stacking classifier

---
+++ task2/stacking_classifier.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import torch
+import dataset
+from sklearn.ensemble import (
+    AdaBoostClassifier,
+    GradientBoostingClassifier,
+    RandomForestClassifier,
+    StackingClassifier,
+)
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+from xgboost import XGBClassifier
+
+X_train = pd.read_csv("data/X_train_2.csv")
+X_test = pd.read_csv("data/X_test_2.csv")
+y_train = pd.read_csv("data/y_train.csv")
+id_col = y_train["id"]
+y_train.drop("id", axis=1, inplace=True)
+
+train_data_base = dataset.TrainDataset(
+    feature_path="data/X_train_2.csv", label_path="data/y_train.csv"
+)
+
+length_train = int(len(train_data_base) * 0.8)
+length_val = len(train_data_base) - length_train
+
+train_data, val_data = torch.utils.data.random_split(train_data_base, [length_train, length_val])
+
+estimators = [
+    ("rf", RandomForestClassifier(max_depth=5, n_estimators=300, max_features=60)),
+    ("svr", make_pipeline(StandardScaler(), LinearSVC(random_state=42))),
+    ("xgb", XGBClassifier(max_depth=5, n_estimators=300, learning_rate=0.05)),
+    (
+        "gbc",
+        GradientBoostingClassifier(
+            max_depth=5, n_estimators=300, learning_rate=0.1, max_features=60
+        ),
+    ),
+    ("hgbc", GradientBoostingClassifier(max_depth=5, n_estimators=500, learning_rate=0.1)),
+    ("ada", AdaBoostClassifier(n_estimators=300, learning_rate=0.1)),
+]
+
+clf = StackingClassifier(estimators=estimators, final_estimator=LogisticRegression())
+
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+
+out = pd.DataFrame()
+out["y"] = y_pred
+out["id"] = id_col
+out.to_csv("out_stacking.csv", index=False)
