9b5df5055fe99dfd83a2014fed506823e27f78f4
niedo <57731234+ni3do@users.noreply.github.com>
2022-12-17 10:46:58 +0100

    beauty changes and corrected val error

--- task3/main.py
+++ task3/main.py
@@ -18,45 +18,36 @@ def train_loop(model, train_loader, loss_fn, optimizer):
         optimizer.step()
 
         if (batch % 10) == 0:
-            print(f"Batch: {batch}, Loss: {loss.item():.4f}")
+            print(f"Batch: {batch}, Loss: {loss.item():.8f}")
             # break
 
 
 def test_loop(model, test_loader, loss_fn, epoch):
-    size = len(test_loader.dataset)
     test_loss = 0
+    size = 0
 
     with torch.no_grad():
         for x, y in test_loader:
             output = model(x)
             test_loss += loss_fn(output, y).item()
+            size += 1
 
     test_loss /= size
-    print(f"Test Error: {test_loss:>8f} \n")
+    print(f"Test Error:     {test_loss:>8f}")
 
     # output = (output > 0.6).float()
 
-    x = x.cpu()
-    y = y.cpu()
-    output = output.cpu()
-
-    utils.produce_gif(x[0].permute(1, 2, 0).detach().numpy(), f"img/input.gif")
-    utils.produce_gif(output[0].permute(1, 2, 0).detach().numpy(), f"img/output.gif")
-    utils.produce_gif(y[0].permute(1, 2, 0).int().detach().numpy(), f"img/label.gif")
+    utils.produce_gif(x[0].permute(1, 2, 0).cpu().detach().numpy(), f"img/input.gif")
+    utils.produce_gif(output[0].permute(1, 2, 0).cpu().detach().numpy(), f"img/output.gif")
+    utils.produce_gif(y[0].permute(1, 2, 0).cpu().detach().numpy(), f"img/label.gif")
 
 
 def main():
     model = UNet(in_channels=1, out_channels=1, init_features=32)
-
-    if DEVICE == "mps":
-        model.float()
-    else:
-        model.double()
-
     model.to(DEVICE)
 
     data_train = dataset.HeartDataset(
-        path="data/train_data_1_256", n_batches=3, unpack_frames=True, device=DEVICE
+        path="data/train_data_1_256", n_batches=4, unpack_frames=True, device=DEVICE
     )
 
     pretrain_length = int(len(data_train) * 0.8)
@@ -65,17 +56,18 @@ def main():
         data_train, [pretrain_length, val_length]
     )
 
-    train_loader = torch.utils.data.DataLoader(data_pretrain, batch_size=8, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(data_val, batch_size=8, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(data_pretrain, batch_size=32, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(data_val, batch_size=32, shuffle=True)
     torch.set_grad_enabled(True)
 
     # loss_fn = torchmetrics.JaccardIndex(num_classes=2)
     loss_fn = loss.JaccardLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-    epochs = 50
+    epochs = 100
 
     for epoch in range(epochs):
+        print(f"------------------------")
         print("Epoch: {}".format(epoch))
         train_loop(model, train_loader, loss_fn, optimizer)
         test_loop(model, val_loader, loss_fn, epoch)
