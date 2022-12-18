c9cf41a443830cde6d2d601d313be8c68688cbed
Yannick Wattenberg <wattenberg@mailbox.org>
2022-12-18 13:46:23 +0100

    create dataset for testdata/submission

--- task3/dataset.py
+++ task3/dataset.py
@@ -17,7 +17,10 @@ class HeartDataset(Dataset):
                     self.data = np.load(f"{path}_{i}.npz", allow_pickle=True)["arr_0"]
                 else:
                     self.data = np.concatenate(
-                        (self.data, np.load(f"{path}_{i}.npz", allow_pickle=True)["arr_0"])
+                        (
+                            self.data,
+                            np.load(f"{path}_{i}.npz", allow_pickle=True)["arr_0"],
+                        )
                     )
         else:
             self.data = np.load(f"{path}_{0}.npz", allow_pickle=True)["arr_0"]
@@ -59,3 +62,56 @@ class HeartDataset(Dataset):
             item["frame"] = torch.Tensor(item["frame"]).to(self.device)
             item["label"] = torch.Tensor(item["label"]).to(self.device)
             return (item["frame"], item["label"])
+
+
+class HeartTestDataset(Dataset):
+    def __init__(self, path, n_batches=1, unpack_frames=False, return_full_data=False, device="cpu"):
+        # path without ending and without batch number
+        # for file test_data_5_112_0.npz the path is test_data_5_112
+        self.return_full_data = return_full_data
+        if n_batches > 1:
+            for i in range(n_batches):
+                if i == 0:
+                    self.data = np.load(f"{path}_{i}.npz", allow_pickle=True)["arr_0"]
+                else:
+                    self.data = np.concatenate(
+                        (
+                            self.data,
+                            np.load(f"{path}_{i}.npz", allow_pickle=True)["arr_0"],
+                        )
+                    )
+        else:
+            self.data = np.load(f"{path}_{0}.npz", allow_pickle=True)["arr_0"]
+        # self.data = utils.load_zipped_pickle(path)
+
+        if unpack_frames:
+            self.data = self.unpack_frames()
+
+        self.device = device
+
+    def unpack_frames(self, data=None):
+        if data is None:
+            data = self.data
+
+        unpacked_data = []
+        for entry in data:
+            for frame in range(len(entry["nmf"][:, 0, 0])):
+                unpacked_data.append(
+                    {
+                        "name": entry["name"],
+                        "frame": entry["nmf"][frame, :, :].numpy().astype(np.float64),
+                    }
+                )
+        return unpacked_data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        if self.return_full_data:
+            return self.data[idx]
+        else:
+            item = self.data[idx]
+            # item = augment_sample(item)
+            item["frame"] = torch.Tensor(item["frame"]).to(self.device)
+            return (item["frame"].unsqueeze(0), None)
