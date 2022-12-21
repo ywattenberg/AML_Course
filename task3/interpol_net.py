c3d422f91aeff5dab07c7c10a27d3a311b07eb48
niedo <57731234+ni3do@users.noreply.github.com>
2022-12-21 17:10:28 +0100

    interpolation works now

--- task3/interpol_net.py
+++ task3/interpol_net.py
@@ -5,7 +5,7 @@ from collections import OrderedDict
 
 class InterpolNet(nn.Module):
     def __init__(self, unet_model, in_channels, out_channels=1, init_features=32) -> None:
-        super().__init__()
+        super(InterpolNet, self).__init__()
 
         if in_channels % 2 == 0:
             raise ValueError("in_channels must be odd")
@@ -17,7 +17,7 @@ class InterpolNet(nn.Module):
         self.in_channels = in_channels
 
         features = init_features
-        self.encoder1 = self._block(in_channels, features, name="enc1")
+        self.encoder1 = self._block(self.in_channels, features, name="enc1")
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.encoder2 = self._block(features, features * 2, name="enc2")
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -28,66 +28,44 @@ class InterpolNet(nn.Module):
 
         self.bottleneck = self._block(features * 8, features * 16, name="bottleneck")
 
-        self.upconv4 = nn.ConvTranspose2d(
-            features * 16, features * 8, kernel_size=2, stride=2
-        )
+        self.upconv4 = nn.ConvTranspose2d(features * 16, features * 8, kernel_size=2, stride=2)
         self.decoder4 = self._block((features * 8) * 2, features * 8, name="dec4")
-        self.upconv3 = nn.ConvTranspose2d(
-            features * 8, features * 4, kernel_size=2, stride=2
-        )
+        self.upconv3 = nn.ConvTranspose2d(features * 8, features * 4, kernel_size=2, stride=2)
         self.decoder3 = self._block((features * 4) * 2, features * 4, name="dec3")
-        self.upconv2 = nn.ConvTranspose2d(
-            features * 4, features * 2, kernel_size=2, stride=2
-        )
+        self.upconv2 = nn.ConvTranspose2d(features * 4, features * 2, kernel_size=2, stride=2)
         self.decoder2 = self._block((features * 2) * 2, features * 2, name="dec2")
-        self.upconv1 = nn.ConvTranspose2d(
-            features * 2, features, kernel_size=2, stride=2
-        )
+        self.upconv1 = nn.ConvTranspose2d(features * 2, features, kernel_size=2, stride=2)
         self.decoder1 = self._block(features * 2, features, name="dec1")
 
-        self.conv = nn.Conv2d(
-            in_channels=features, out_channels=out_channels, kernel_size=1
-        )
-
+        self.conv = nn.Conv2d(in_channels=features, out_channels=out_channels, kernel_size=1)
 
     def forward(self, x):
-        for i in range(self.in_channels):
-            if i == 0:
-                tmp = self.self_model(x[i])
-            else:
-                tmp = torch.cat((tmp, self.self_model(x[i])), dim=1)
-
-        x = tmp
         enc1 = self.encoder1(x)
         enc2 = self.encoder2(self.pool1(enc1))
-        # enc3 = self.encoder3(self.pool2(enc2))
-        # enc4 = self.encoder4(self.pool3(enc3))
+        enc3 = self.encoder3(self.pool2(enc2))
+        enc4 = self.encoder4(self.pool3(enc3))
 
-        bottleneck = self.bottleneck(self.pool1(enc2))
+        bottleneck = self.bottleneck(self.pool4(enc4))
 
-        # bottleneck = self.bottleneck(self.pool4(enc4))
-
-        # dec4 = self.upconv4(bottleneck)
-        # dec4 = torch.cat((dec4, enc4), dim=1)
-        # dec4 = self.decoder4(dec4)
-        # dec3 = self.upconv3(dec4)
-        # dec3 = torch.cat((dec3, enc3), dim=1)
-        # dec3 = self.decoder3(dec3)
-        dec2 = self.upconv2(bottleneck)
+        dec4 = self.upconv4(bottleneck)
+        dec4 = torch.cat((dec4, enc4), dim=1)
+        dec4 = self.decoder4(dec4)
+        dec3 = self.upconv3(dec4)
+        dec3 = torch.cat((dec3, enc3), dim=1)
+        dec3 = self.decoder3(dec3)
+        dec2 = self.upconv2(dec3)
         dec2 = torch.cat((dec2, enc2), dim=1)
         dec2 = self.decoder2(dec2)
         dec1 = self.upconv1(dec2)
-        # dec1 = self.upconv1(bottleneck)
         dec1 = torch.cat((dec1, enc1), dim=1)
         dec1 = self.decoder1(dec1)
-        return torch.sigmoid(self.conv(dec1))
-        # return torch.sigmoid(self.conv(dec1))
-    
+        sigm = torch.sigmoid(self.conv(dec1))
+        if self.training:
+            return sigm
+        else:
+            return torch.where(sigm > 0.9, 1, 0)
 
-
-
-
-    def _block(in_channels, features, name):
+    def _block(self, in_channels, features, name):
         return nn.Sequential(
             OrderedDict(
                 [
